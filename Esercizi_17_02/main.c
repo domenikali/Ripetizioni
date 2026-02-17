97982ea74781ba8c1b38eb968e0bee5f0d64a836
Leonardo Domenicali <104627136+domenikali@users.noreply.github.com>
2026-02-17 18:28:14 +0100

    aggiunta esercizi

--- Esercizi_17_02/main.c
+++ Esercizi_17_02/main.c
@@ -10,7 +10,31 @@ void dispari (int partenza,int arrivo){
 
 }
 
+//stampa la somma dei numeri da 0 a N
+void somma(int n){
 
+}
+
+//conta (e stampa) il numero di divisori (intera e senza resto) di n
+//es: 10 ha come divisori 1,2,5,(10?) -> 3/4 divisori (sciegli te se considerare anche il numero stesso)
+void divisori(int n){
+
+}
+
+//verifica se un numero è primo
+void primo(int n){
+
+}
+
+//DIFFICILE! (cicli innestati ma ce la puoi fare!)
+//stampa un triangolo di n pallini
+// n=3->
+// *
+// **
+// ***
+void triangolo(int n){
+
+}
 
 int main(){
 
