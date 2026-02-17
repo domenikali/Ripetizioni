2ce7d63154c4ba5865174a2a781a80ba490be3fc
Leonardo Domenicali <leonardo.domenicali@gmail.com>
2026-02-17 12:42:51 +0100

    primi es del 17

---
+++ Esercizi_17_02/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+#include <stdint.h>
+
+
+//stampa i numeri dispari da partenza ad arrivo (compresi o esclusi a scelta)
+void dispari (int partenza,int arrivo){
+
+}
+
+
+
+int main(){
+
+    dispari(0,10);
+    return 0;
+
+}
